0637ca1316e8b66a7219203f9c26fab97ab2af78
Alex Kotov <kotovalexarian@gmail.com>
2022-12-06 20:03:24 +0400

    Rename build directory

--- make/libkernaux.mk
+++ make/libkernaux.mk
@@ -1,12 +1,12 @@
-dest/usr/include/kernaux.h: build/libkernaux/main/Makefile
-	$(MAKE) -C build/libkernaux/main DESTDIR='$(ABS_DEST)' install-data
+dest/usr/include/kernaux.h: build/libkernaux/Makefile
+	$(MAKE) -C build/libkernaux DESTDIR='$(ABS_DEST)' install-data
 
-dest/usr/lib/libkernaux.la: build/libkernaux/main/Makefile
-	$(MAKE) -C build/libkernaux/main DESTDIR='$(ABS_DEST)' install-exec
+dest/usr/lib/libkernaux.la: build/libkernaux/Makefile
+	$(MAKE) -C build/libkernaux DESTDIR='$(ABS_DEST)' install-exec
 
-build/libkernaux/main/Makefile: vendor/libkernaux/configure
-	mkdir -p build/libkernaux/main
-	cd build/libkernaux/main && '../../../vendor/libkernaux/configure' --prefix=/usr
+build/libkernaux/Makefile: vendor/libkernaux/configure
+	mkdir -p build/libkernaux
+	cd build/libkernaux && '../../../vendor/libkernaux/configure' --prefix=/usr
 
 vendor/libkernaux/configure:
 	cd vendor/libkernaux && ./autogen.sh
